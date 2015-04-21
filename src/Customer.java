439a9530b5dd363f98ab6a13487d22f392241460
spohnle <leonspohn@gmx.de>
2015-04-21 13:11:24 +0200

    htmlStatement() hinzugefügt

--- src/Customer.java
+++ src/Customer.java
@@ -56,5 +56,23 @@ class Customer {
     	return result;
     }
     
+    public String htmlStatement() {
+    	Enumeration enum_rentals = rentals.elements();
+    	String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+    	while (enum_rentals.hasMoreElements()) {
+    		Rental each = (Rental) enum_rentals.nextElement();
+	    	//show figures for each rental
+	    	result += each.getMovie().getTitle()+ ": " +
+	    	String.valueOf(each.getCharge()) + "<BR>\n";
+    	}
+    	//add footer lines
+    	result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) +
+    	"</EM><P>\n";
+    	result += "On this rental you earned <EM>" +
+    	String.valueOf(getTotalFrequentRenterPoints()) +
+    	"</EM> frequent renter points<P>";
+    	return result;
+    	}
+    
 }
     
